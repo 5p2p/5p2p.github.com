1f879713c5d8343ef521c00311fe6a834676720b
5p2p <ruvido@gmail.com>
2013-04-24 20:49:37 +0200

    mobile design

--- js/resolution-test.js
+++ js/resolution-test.js
@@ -1,15 +1,22 @@
 function adjustStyle(width) {
-    width = parseInt(width);
-    if (width < 701) {
-        $("#size-stylesheet").attr("href", "css/mobile.css");
-    } else {
-       $("#size-stylesheet").attr("href", "css/style.css"); 
-    }
+  width = parseInt(width);
+  if (width < 931) {
+    $("#size-stylesheet").attr("href", "/css/mobile.css");
+  } else {
+    $("#size-stylesheet").attr("href", "/css/style.css"); 
+  }
 }
 
 $(function() {
+  adjustStyle($(this).width());
+  $(window).resize(function() {
     adjustStyle($(this).width());
-    $(window).resize(function() {
-        adjustStyle($(this).width());
-    });
+  });
+});
+
+$(function() {
+  adjustStyle($(this).width());
+  $(window).resize(function() {
+    adjustStyle($(this).width());
+  });
 });
